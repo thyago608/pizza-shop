f86cb3baac3b829e604fcc1e58122e5e32df419b
Thyago Ribeiro <thyago@brickseguros.com.br>
2024-07-19 07:49:42 -0300

    feat: Create Page SignUp

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Helmet } from 'react-helmet-async'
+import { Link } from 'react-router-dom'
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -39,6 +40,10 @@ export function SignIn() {
       <Helmet title="Login" />
 
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,26 +3,20 @@ import { AppLayout } from './pages/_layouts/app'
 import { Dashboard } from './pages/app/dashboard'
 import { AuthLayout } from './pages/_layouts/auth'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    children: [
-      {
-        path: '/',
-        element: <Dashboard />,
-      },
-    ],
+    children: [{ path: '/', element: <Dashboard /> }],
   },
   {
     path: '/',
     element: <AuthLayout />,
     children: [
-      {
-        path: '/sign-in',
-        element: <SignIn />,
-      },
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
     ],
   },
 ])
