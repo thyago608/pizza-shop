1431a680fc92a2d38d2c035038132d22c15de67f
Thyago Ribeiro <thyago@brickseguros.com.br>
2024-07-22 08:19:13 -0300

    feat: Add Theme Toggle

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { Toaster } from 'sonner'
 import { router } from './routes'
 import './global.css'
+import { ThemeProvider } from './components/theme/theme-provider'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="light" storageKey="pizzashop-theme">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -2,9 +2,9 @@ import { Helmet } from 'react-helmet-async'
 
 export function Dashboard() {
   return (
-    <>
+    <div className="bg-foreground">
       <Helmet title="Dashboard" />
       <h1>dashboard</h1>
-    </>
+    </div>
   )
 }
