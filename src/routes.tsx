b417451b7113f0296abedb1ab7167b9ae97fce63
Thyago Ribeiro <thyago@brickseguros.com.br>
2024-07-22 08:30:26 -0300

    feat: Create Page Orders

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,12 +4,19 @@ import { Dashboard } from './pages/app/dashboard'
 import { AuthLayout } from './pages/_layouts/auth'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
+import { Orders } from './pages/app/orders'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    children: [{ path: '/', element: <Dashboard /> }],
+    children: [
+      { path: '/', element: <Dashboard /> },
+      {
+        path: '/orders',
+        element: <Orders />,
+      },
+    ],
   },
   {
     path: '/',
