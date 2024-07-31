a51d8c18e58f0406fa2657311308116b11daf11b
Thyago Ribeiro <thyago@brickseguros.com.br>
2024-07-30 22:07:38 -0300

    fix: ThemeToggle Provider

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -2,7 +2,7 @@ import { Helmet } from 'react-helmet-async'
 
 export function Dashboard() {
   return (
-    <div className="bg-foreground">
+    <div className="">
       <Helmet title="Dashboard" />
       <h1>dashboard</h1>
     </div>
