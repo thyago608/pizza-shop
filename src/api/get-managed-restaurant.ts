7135ab8d182f893d34f0fe848c70e6621f11af25
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2024-08-22 18:55:24 -0300

    feat: Update ManagedRestaurant cache

--- src/api/get-managed-restaurant.ts
+++ src/api/get-managed-restaurant.ts
@@ -1,6 +1,6 @@
 import { api } from '@/lib/axios'
 
-type GetManagedRestaurantResponse = {
+export type GetManagedRestaurantResponse = {
   id: string
   name: string
   role: 'manager' | 'customer'
